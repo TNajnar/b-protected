b6a188356f7889f63e25b417b8eecd6ada208e11
Tomáš Najnar <tomas.najnar@seznam.cz>
2025-08-14 16:21:43 +0200

    Added translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslatePipe } from "@ngx-translate/core";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, TranslatePipe],
     templateUrl: './app.component.html',
 })
 export class AppComponent {
-    title = 'B-Protected';
+    title = 'APP.Title';
 }
