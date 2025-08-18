830000e0b2bf1579eca2f876e4e3177253a189ef
Tomáš Najnar <tomas.najnar@seznam.cz>
2025-08-18 20:01:11 +0200

    Contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
+import { TranslatePipe } from '@ngx-translate/core';
+
+import { COMPANY_CONSTANTS } from '@common/constants';
 
 @Component({
-  selector: 'app-contact',
-  standalone: true,
-  imports: [ButtonModule, InputTextModule, FormsModule],
-  templateUrl: './contact.component.html'
+    selector: 'app-contact',
+    standalone: true,
+    imports: [ButtonModule, InputTextModule, FormsModule, TranslatePipe],
+    templateUrl: './contact.component.html'
 })
-export class ContactComponent { }
+export class ContactComponent {
+    protected readonly CONSTANTS = COMPANY_CONSTANTS;
+}
---
+++ src/common/constants.ts
@@ -0,0 +1,9 @@
+export const COMPANY_CONSTANTS = {
+  ADDRESS_VALUE: 'Praha, Česká republika',
+  PHONE_VALUE: '+420 XXX XXX XXX',
+  EMAIL_VALUE: 'info@b-protected.cz',
+
+  COMPANY_NAME_VALUE: 'B-Protected Services s.r.o.',
+  DIRECTOR_VALUE: 'Ing. Tomáš Najnar',
+  ICO_VALUE: '123 45 678'
+} as const;
